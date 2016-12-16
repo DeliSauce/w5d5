8b3b08fea47fcfb2c41b27308a98e0209289d35b
App Academy Student <student@appacademy.io>
2016-12-16 14:07:08 -0800

    add variation to prototyped curry method

--- curriedsum.js
+++ curriedsum.js
@@ -1,3 +1,4 @@
+// //implementation of curry
 // function curriedSum(numArgs) {
 //   let numbers = [];
 //   function _curriedSum(num){
@@ -10,30 +11,55 @@
 //   }
 //   return _curriedSum;
 // }
-
-
-function sum(arr) {
-  let total = 0;
-  arr.forEach(el => {total += el;});
-  return total;
-}
-
+//
+//
+// function sum(arr) {
+//   let total = 0;
+//   arr.forEach(el => {total += el;});
+//   return total;
+// }
+//
 // const curry = curriedSum(3);
 // console.log(curry(2));
 // console.log(curry(5));
 // console.log(curry(3));
-
-
+//
+//
 // console.log(sum([1,2,3,4]));
+//
+// //Monkey patched version of curry (with the spread operator)
+// Function.prototype.curry = function(numArgs) {
+//   let args = [];
+//   let func = this;
+//   function _curriedSum(num){
+//     args.push(num);
+//     if (args.length === numArgs) {
+//       return func(...args);
+//     } else {
+//       return _curriedSum;
+//     }
+//   }
+//   return _curriedSum;
+// };
+//
+//
+// function sumThree(num1, num2, num3) {
+//   return num1 + num2 + num3;
+// }
+//
+// sumThree(4, 20, 6); // == 30
+// sumThree.curry(3)(4)(20)(6); // == 30
 
 
+
+//Monkey patched version of curry (with the apply function)
 Function.prototype.curry = function(numArgs) {
   let args = [];
   let func = this;
-  function _curriedSum(num){
-    args.push(num);
+  function _curriedSum(arg){
+    args.push(arg);
     if (args.length === numArgs) {
-      return func(...args);
+      return func.apply(null, args);
     } else {
       return _curriedSum;
     }
@@ -47,4 +73,4 @@ function sumThree(num1, num2, num3) {
 }
 
 sumThree(4, 20, 6); // == 30
-sumThree.curry(3)(4)(20)(6); // == 30
+console.log(sumThree.curry(3)(4)(20)(6)); // == 30
